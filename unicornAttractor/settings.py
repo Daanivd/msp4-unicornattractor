6752d8dc9d749cd1bd25a4b1c3bc7b94ed45785b
Ubuntu <ubuntu@ip-172-31-35-5.ec2.internal>
2019-07-25 08:32:59 +0000

    installed dj_database_url and set up if statement for importing env.py in settings

--- unicornAttractor/settings.py
+++ unicornAttractor/settings.py
@@ -11,8 +11,14 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 """
 
 import os
-import env
 import dj_database_url
+
+if "DATABASE_URL" in os.environ:
+    print("Database URL found --> Not importing env.py")
+else: 
+    import env
+
+
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
