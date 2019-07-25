bbd738c82711e2afdaa0288e5bf6de4ac4e105e3
Ubuntu <ubuntu@ip-172-31-35-5.ec2.internal>
2019-07-25 07:50:16 +0000

    added S3 bucket functionality with django-storage and boto3

--- unicornAttractor/settings.py
+++ unicornAttractor/settings.py
@@ -30,7 +30,6 @@ ALLOWED_HOSTS = [os.environ.get('C9_HOST')]
 
 
 # Application definition
-
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
@@ -39,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django_forms_bootstrap',
+    'storages',
     'home',
     'accounts',
     'tickets',
@@ -131,12 +131,30 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
+# AWS_S3_OBJECT_PARAMETERS = {
+#     'Expires': 'Thu, 31 Dec 2099 20:00:00 GMT',
+#     'CacheControl': 'max-age=94608000'
+# }
+
+# AWS_STORAGE_BUCKET_NAME = 'unicornattractor'
+# AWS_S3_REGION_NAME = 'eu-central-1'
+# AWS_ACCESS_KEY_ID = os.environ.get("AWS_SECRET_KEY_ID")
+# AWS_SECRET_ACCESS_KEY = os.environ.get("AWS_SECRET_ACCESS_KEY")
+
+# AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
+
+# STATICFILES_LOCATION = 'static'
+# STATICFILES_STORAGE = 'custom_storages.StaticStorage'
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'), )
 
+# MEDIAFILES_LOCATION = 'media'
+# DEFAULT_FILE_STORAGE = 'custom_storages.MediaStorage'
+
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+# # STATICFILES_DIRS = (os.path.join(BASE_DIR, 'media'), )
 
 MESSAGE_STORAGE = "django.contrib.messages.storage.session.SessionStorage"
 
