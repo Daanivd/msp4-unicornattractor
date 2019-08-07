8d52fa9d7a914bfdee8a7625b34169b200482165
Ubuntu <ubuntu@ip-172-31-35-5.ec2.internal>
2019-08-07 10:50:32 +0000

    created test_views.py in search app for 100% coverage

---
+++ search/test_views.py
@@ -0,0 +1,17 @@
+from django.test import TestCase
+from django.contrib.auth.models import User
+from tickets.models import Ticket
+
+
+
+class TestSearch(TestCase):
+
+    def test_search_(self):
+        user = User.objects.create_user(username='test_user', password='password')
+        self.client.login(username='test_user', password='password')
+        ticket = Ticket(ticketName="Test Ticket", description='test-content', author=user)
+        ticket.save()
+        self.client.post
+        page = self.client.get("/search/?query=unicorn",  follow=True)
+        self.assertEqual(page.status_code, 200)
+        self.assertTemplateUsed(page, "tickets.html")
