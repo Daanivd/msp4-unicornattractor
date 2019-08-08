8dd49d3f22e02ce661d9a45d4269f2a60766f13e
Ubuntu <ubuntu@ip-172-31-35-5.ec2.internal>
2019-08-08 14:56:55 +0000

    added C3.js graphs on stats for resolving of bugs and developing new features on index template

--- home/views.py
+++ home/views.py
@@ -1,8 +1,14 @@
 from django.shortcuts import render
+from django.core import serializers
 from features.models import Feature
 from tickets.models import Ticket
 from features.forms import featureForm
 from cart.views import add_to_cart
+from django.utils import timezone
+from django.http import JsonResponse
+import json
+
+
 
 # Create your views here.
 def index(request):
@@ -11,6 +17,54 @@ def index(request):
     productFeatures = Feature.objects.filter(status=2)
     devFeatures = Feature.objects.filter(status=3)
     tickets = Ticket.objects.all()
-   
+  
+    bugData = get_bugData()
+    featureDevData = get_featureData()
     
-    return render(request, "index.html", {"features": features, 'productFeatures': productFeatures, "devFeatures": devFeatures, 'tickets': tickets})
+    return render(request, "index.html", {"features": features, 'productFeatures': productFeatures, "devFeatures": devFeatures, 'tickets': tickets, 'featureDevData': featureDevData, 'bugData': bugData})
+
+
+def get_bugData():
+    bugs =  Ticket.objects.filter(status=3) 
+    bugs_list = []
+    for ticket in bugs:
+        deltaT = ticket.fixed_date-ticket.published_date
+        bugs_list.append(round(deltaT.total_seconds()/3600/24))
+      
+    bugs_list_set = list(set(bugs_list))
+    count_list = ['Days to Resolve Bugs',]
+    for n in bugs_list_set:
+        count_list.append(bugs_list.count(n))
+        
+    bugT = ['bugT']  
+    for bug in bugs_list_set:
+        bugT.append(bug)
+        
+    bugData = [bugT, count_list]     
+    
+    return bugData
+    
+def get_featureData():  
+    features = Feature.objects.filter(status=4)
+    # flist = ['feature',]
+    dlist = ['Timeline',]
+    nlist = ['Number of Features over Time',]
+    
+    for feature in features:
+        # flist.append(feature.featureName)
+        dlist.append(str(feature.feature_added_date)[0:19])
+    for i in range(0, len(dlist)-1):
+        nlist.append(i+1)
+       
+    featureDevData = [dlist, nlist]   
+    
+    return featureDevData
+    
+    
+    
+
+def fdata(request):
+    fdata = Feature.objects.all()
+    fdata = serializers.serialize('json', fdata)
+    d = json.loads(fdata)
+    return JsonResponse(d, safe=False)
---
+++ static/C3/charts.js
@@ -0,0 +1,61 @@
+           var chart = c3.generate({
+                bindto: '#featureChart',
+    data: {
+        x: 'Timeline',
+        xFormat: '%Y-%m-%d %H:%M:%S',
+        columns: {{featureDevData|safe}}
+    },
+    axis: {
+        x: {
+            type: 'timeseries',
+            tick: {
+                format: '%Y-%m-%d'
+            }
+        },
+        y: {
+            tick:{
+            values: [1,2,3,4]
+            }
+            
+        }
+    },
+    color: {
+        pattern: ['pink']
+    }
+});
+
+var histogram = c3.generate({
+bindto: '#bugHistogram',
+    data: {
+        x: 'bugT',
+        columns: {{bugData|safe}},
+        type: 'bar'
+        
+    },
+    axis: {
+        x: {
+            type: 'bar',
+            tick: {
+                
+            }
+        },
+        y: {
+            tick:{
+            values: [1,2,3,4]
+            }
+            
+        }
+    },
+    bar: {
+        width: {
+            ratio: 0.15 
+        }
+    
+    },
+    
+    color: {
+        pattern: ['pink']
+    }
+    
+});
+    
